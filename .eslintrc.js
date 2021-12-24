b798094f61b2057abd5ada39f55bd9a7a74edd25
pujianguo <18919015125@163.com>
2021-12-24 17:24:49 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'standard',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+  globals: {
+    defineProps: true,
+  },
+}
