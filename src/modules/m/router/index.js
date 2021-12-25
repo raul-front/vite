b0b74818a100c03b312b6e3525ecee017d1b26de
pujianguo <18919015125@163.com>
2021-12-25 20:34:05 +0800

    update router mode

--- src/modules/m/router/index.js
+++ src/modules/m/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from 'm/views/home/index.vue'
 
 const router = createRouter({
-  history: createWebHistory('/m'),
+  history: createWebHashHistory('/m'),
   routes: [
     {
       path: '',
