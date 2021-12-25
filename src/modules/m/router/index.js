71af9d713b71a02c79b2ee3d1accded0b0bf1ceb
pujianguo <18919015125@163.com>
2021-12-25 18:53:16 +0800

    update: history模式

--- src/modules/m/router/index.js
+++ src/modules/m/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Home from 'm/views/home/index.vue'
 
 const router = createRouter({
-  history: createWebHashHistory('/m'),
+  history: createWebHistory('/m'),
   routes: [
     {
       path: '',
