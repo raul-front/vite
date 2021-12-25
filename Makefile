48f33a1c126776403cfa6ba433cc559bf1906ed6
pujianguo <18919015125@163.com>
2021-12-25 20:28:16 +0800

    add ci

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
+BUCKET=raul-demo
+PROJECT=vite
+
+
 install:
 	rm -rf node_modules
 	rm -f npm-lock.yaml
@@ -7,8 +11,20 @@ build-release:
 	rm -rf dist
 	pnpm build:release
 
-release:
-	git checkout release
-	git merge master
-	git push origin release
-	git checkout master
+# release:
+# 	git checkout release
+# 	git merge master
+# 	git push origin release
+# 	git checkout master
+
+# github action 自动部署
+deploy:
+	curl https://devtools.qiniu.com/qshell-v2.6.1-linux-amd64.tar.gz | tar zxv; \
+	chmod +x qshell; \
+	./qshell account ${AK} ${SK} raul; \
+	# 删除原文件
+	./qshell listbucket2 --prefix '${PROJECT}/' ${BUCKET} -o todelete.txt
+	./qshell batchdelete --force ${BUCKET} -i todelete.txt
+	rm todelete.txt
+	# 上传新文件
+	./qshell qupload2 --src-dir=./dist/ --overwrite --bucket=${BUCKET}
--- vite.config.js
+++ vite.config.js
@@ -2,46 +2,55 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  build: {
-    rollupOptions: {
-      input: {
-        pc: resolve(__dirname, 'index.html'),
-        m: resolve(__dirname, 'm.html'),
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      styles: resolve(__dirname, 'src/styles'),
-      hooks: resolve(__dirname, 'src/hooks'),
-      components: resolve(__dirname, 'src/components'),
-      utils: resolve(__dirname, 'src/utils'),
-      api: resolve(__dirname, 'src/api'),
-      pc: resolve(__dirname, 'src/modules/pc'),
-      m: resolve(__dirname, 'src/modules/m'),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        javascriptEnabled: true,
-        additionalData: (content, loaderContext) => {
-          return "@import 'styles/mixins.scss';" + content
+export default ({ mode }) => {
+  const isDev = mode === 'development'
+  const projectName = 'vite'
+
+  return defineConfig({
+    plugins: [vue()],
+    base: isDev ? '/' : `/${projectName}/`,
+
+    // 为了打包后能访问到静态图片，将public设置为echarts
+    publicDir: projectName,
+    build: {
+      outDir: `dist/${projectName}`,
+      rollupOptions: {
+        input: {
+          pc: resolve(__dirname, 'index.html'),
+          m: resolve(__dirname, 'm.html'),
         },
       },
     },
-  },
-  server: {
-    proxy: {
-      '/devApi': {
-        target: 'http://localhost:7001',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/devApi/, '/'),
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+        styles: resolve(__dirname, 'src/styles'),
+        hooks: resolve(__dirname, 'src/hooks'),
+        components: resolve(__dirname, 'src/components'),
+        utils: resolve(__dirname, 'src/utils'),
+        api: resolve(__dirname, 'src/api'),
+        pc: resolve(__dirname, 'src/modules/pc'),
+        m: resolve(__dirname, 'src/modules/m'),
       },
     },
-  },
-})
+    css: {
+      preprocessorOptions: {
+        scss: {
+          javascriptEnabled: true,
+          additionalData: (content, loaderContext) => {
+            return "@import 'styles/mixins.scss';" + content
+          },
+        },
+      },
+    },
+    server: {
+      proxy: {
+        '/devApi': {
+          target: 'http://localhost:7001',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/devApi/, '/'),
+        },
+      },
+    },
+  })
+}
